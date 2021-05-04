e9265218341a995b5165d3107243cda564f34521
Joao Victor Fernandes <jovic258.fernandes@hotmail.com>
2021-05-04 04:27:32 -0300

    first commit

---
+++ app/models/pin.rb
@@ -0,0 +1,13 @@
+class Pin < ApplicationRecord
+    has_many_attached :images
+    validates :title, presence: true
+    validates :caption, presence: true 
+    validate :image_type
+
+    private
+    def image_type
+        if images.attached? == false
+            errors.add(:images, "can't be empty")
+        end
+    end
+end
